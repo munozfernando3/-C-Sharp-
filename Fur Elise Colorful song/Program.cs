04b83b876a8684542ae7bcf1dd1e5085d18b1f0c
munozfernando3 <fernando.munoz071943@gmail.com>
2023-11-04 02:58:07 -0600

    New Files

---
+++ Fur Elise Colorful song/Program.cs
@@ -0,0 +1,391 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fur Elise");
+
+int A = 440;
+int B = 494;
+int C = 262;
+int D = 294;
+int E = 330;
+int F =349;
+int G= 392;
+int Eb = 311;
+int Ab= 415;
+int wholenote= 2000;
+
+Console.Beep(E +340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb+320, wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb +320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(B+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(D+293,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(C+262,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(A+5,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(C,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(A+30,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(B+20, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Ab+100,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkYellow;
+Console.Clear();
+
+Console.Beep(B+1,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(A-5,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(E,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb+320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb+320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(B+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(D+293,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(C+262,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(A+5,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(C,wholenote/4);
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(A+30,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(B+20, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Ab+100,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkYellow;
+Console.Clear();
+
+Console.Beep(B+1,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(A-5, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(E +340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb+320, wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb +320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(B+20,wholenote/4);
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(D+293,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(C+262,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(A+5,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(C,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(A+30,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(B+10, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Ab+100,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkYellow;
+Console.Clear();
+
+Console.Beep(B+1,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(A-5, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(B+100,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(C+370,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(D+370,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(G+168,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Cyan;
+Console.Clear();
+
+Console.Beep(F+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.White;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(D+292,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(F+50,wholenote/4);
+Console.BackgroundColor=ConsoleColor.White;
+Console.Clear();
+
+Console.Beep(E+320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(D+293,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(C+262,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(E+20, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(D+293,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(C+262,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(B+10, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(E+340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(E, wholenote/2000);
+Console.Beep(E, wholenote/2000);
+Console.Beep(E, wholenote/2000);
+Console.Beep(E, wholenote/2000);
+Console.Beep(E+340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(E+340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(E+550, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb +320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E +340,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(E +340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+
+Console.Beep(E +340, wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb+320, wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Eb +320,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.Beep(E+340,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(B+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(D+293,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Red;
+Console.Clear();
+
+Console.Beep(C+262,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+Console.Beep(A+5,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(C,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Magenta;
+Console.Clear();
+
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(A+30,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(B+20, wholenote/2);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(E+20,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
+
+Console.Beep(Ab+100,wholenote/4);
+Console.BackgroundColor=ConsoleColor.DarkYellow;
+Console.Clear();
+
+Console.Beep(B+1,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Clear();
+
+Console.Beep(A-5,wholenote/2);
+Console.BackgroundColor=ConsoleColor.Yellow;
+Console.Clear();
+
+Console.Beep(E,wholenote/4);
+Console.BackgroundColor=ConsoleColor.Blue;
+Console.Clear();
---
+++ Random People Generator/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+public class MyApp
+{
+    public static void Main(string[] args)
+    {
+        const int result = 1000;
+
+        string[] firstNameResult = new string[result];
+        string[] middleNameResult = new string[result];
+        string[] lastNameResult = new string[result];
+        string[] jobResult = new string[result];
+        decimal[] hourlyWageResult = new decimal[result];
+        decimal[] yearlySalaryResult = new decimal[result];
+
+        string[] MaleFirstNames = new string[10] { "Jack", "Charles", "James", "William", "John", "Rickard", "Andrew", "Alex", "Harry", "Roberth" };
+        string[] MaleMiddleNames = new string[10] { "Henrry", "Mateo", "Daniel", "Owen", "Samuel", "Jacob", "Asher", "Joseph", "David", "Leo" };
+        string[] FemaleFirstNames = new string[10] { "Anna", "Eva", "Lorraine", "Sophia", "Doris", "Mary", "Elizabeth", "Brienne", "Maya", "Alexia" };
+        string[] FemaleMiddleNames = new string[10] { "Maeve", "Alice", "Ayleen", "Scarlett", "Diana", "Rose", "Arya", "Madison", "Willow", "Grace" };
+        string[] LastNames = new string[10] { "Thompson", "Robinson", "Wittmark", "Brown", "Fox", "Wallace", "Nielsom", "Anderson", "Hills", "Garcia" };
+
+        string[] job = new string[] { "Professor", "Singer", "Pianist", "Lawyer", "Software Engineer", "Doctor", "Waiter", "Accountant", "Chef", "Writer" };
+        decimal[] hourlyWage = new decimal[] { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+        decimal[] yearlySalary = new decimal[10];
+        var random = new Random();
+        for (int i = 0; i < result; i++)
+        {
+            string randomMaleFirstNames = MaleFirstNames[random.Next(MaleFirstNames.Length)];
+            string randomMaleMiddleNames = MaleMiddleNames[random.Next(MaleMiddleNames.Length)];
+            string randomLastNames = LastNames[random.Next(LastNames.Length)];
+            while (!Name(randomMaleFirstNames, randomMaleMiddleNames, randomLastNames, firstNameResult, middleNameResult, lastNameResult, i))
+            {
+                randomMaleFirstNames = MaleFirstNames[random.Next(MaleFirstNames.Length)];
+                randomMaleMiddleNames = MaleMiddleNames[random.Next(MaleMiddleNames.Length)];
+                randomLastNames = LastNames[random.Next(LastNames.Length)];
+            }
+            firstNameResult[i] = randomMaleFirstNames;
+            middleNameResult[i] = randomMaleMiddleNames;
+            lastNameResult[i] = randomLastNames;
+            jobResult[i] = job[random.Next(job.Length)];
+            hourlyWageResult[i] = hourlyWage[random.Next(hourlyWage.Length)];
+
+            i++;
+            string randomFirstFemales = FemaleFirstNames[random.Next(FemaleFirstNames.Length)];
+            string randomMiddleNameFemales = FemaleMiddleNames[random.Next(FemaleMiddleNames.Length)];
+            randomLastNames = LastNames[random.Next(LastNames.Length)];
+
+            while (!Name(randomFirstFemales, randomMiddleNameFemales, randomLastNames, firstNameResult, middleNameResult, lastNameResult, i))
+            {
+                randomFirstFemales = FemaleFirstNames[random.Next(FemaleFirstNames.Length)];
+                randomMiddleNameFemales = FemaleMiddleNames[random.Next(FemaleMiddleNames.Length)];
+                randomLastNames = LastNames[random.Next(LastNames.Length)];
+
+            }
+
+            firstNameResult[i] = randomFirstFemales;
+            middleNameResult[i] = randomMiddleNameFemales;
+            lastNameResult[i] = randomLastNames;
+            jobResult[i] = job[random.Next(job.Length)];
+            hourlyWageResult[i] = hourlyWage[random.Next(hourlyWage.Length)];
+        }
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine(i+1+ ". "+firstNameResult[i] + " " + middleNameResult[i] + " " + lastNameResult[i] + " ");
+            Console.WriteLine("Job: " + jobResult[i]);
+            Console.WriteLine(hourlyWageResult[i] + " per hour.");
+            Console.WriteLine(hourlyWageResult[i] * 40 * 50 + " per year");
+            Console.WriteLine("------------------------------------------------------");
+        }
+
+    }
+    private static bool Name(string first, string mid, string last, string[] firstresult, string[] midresult, string[] lastresult, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (first == firstresult[i] && mid == midresult[i] && last == lastresult[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}   
+            
+            
+            
